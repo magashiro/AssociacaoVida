c22629753d93b3231de739e980d215ac0e713997
Pedro Thomas Chaib Cintra <Pedro Thomas Chaib Cintra>
2018-05-27 17:19:58 -0300

    small changes and animal validation fields

--- src/pages/animalForm/animalForm.ts
+++ src/pages/animalForm/animalForm.ts
@@ -25,20 +25,43 @@ homePage = HomePage;
   }
 
   save(){
-    this.navCtrl.push(UserFormPage, {
-      nome: this.model.nome,
-      tipo: this.model.tipo,
-      sexo: this.model.sexo,
-      anos: this.model.anos,
-      meses: this.model.meses,
-      porte: this.model.porte,
-      temperamento: this.model.temperamento,
-      raca: this.model.raca,
-      vacinado: this.model.vacinado,
-      castrado: this.model.castrado,
-      info: this.model.info,
-      img: this.model.img
-    });
+    if (this.validateFields()){
+      this.navCtrl.push(UserFormPage, {
+        nome: this.model.nome,
+        tipo: this.model.tipo,
+        sexo: this.model.sexo,
+        anos: this.model.anos,
+        meses: this.model.meses,
+        porte: this.model.porte,
+        temperamento: this.model.temperamento,
+        raca: this.model.raca,
+        vacinado: this.model.vacinado,
+        castrado: this.model.castrado,
+        info: this.model.info,
+        img: this.model.img
+      });
+    }
   }
 
+  validateFields(){
+    this.ruleValidateFields(this.model.nome,null,3);
+    this.ruleValidateFields(this.model.tipo,null,1);
+    this.ruleValidateFields(this.model.sexo,null,1);
+    this.ruleValidateFields(this.model.anos,null,1);
+    this.ruleValidateFields(this.model.meses,null,1);
+    this.ruleValidateFields(this.model.porte,null,1);
+    this.ruleValidateFields(this.model.temperamento,null,1);
+    this.ruleValidateFields(this.model.raca,null,3);
+    this.ruleValidateFields(this.model.vacinado,null,1);
+    this.ruleValidateFields(this.model.castrado,null,1);
+    this.ruleValidateFields(this.model.img,null,0);
+  }
+
+  ruleValidateFields(field,type,minChar){
+    if(field != '' && field != undefined && field.length >= minChar){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
