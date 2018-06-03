848ccb8599a6a7ecc1c4e2e41fed55a6ef610e1b
Pedro Thomas Chaib Cintra <Pedro Thomas Chaib Cintra>
2018-06-03 03:00:05 -0300

    new user table

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -26,6 +26,7 @@ export class DatabaseProvider {
 
       private createTables(db: SQLiteObject){
         db.sqlBatch([
+          ['CREATE TABLE IF NOT EXISTS usuario (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, login TEXT, senha TEXT)'],
           ['CREATE TABLE IF NOT EXISTS animal (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, status TEXT, tipo TEXT, nome TEXT, sexo TEXT, anos INTEGER, meses INTEGER, porte TEXT, temperamento TEXT, raca TEXT, vacinado TEXT, castrado TEXT, info TEXT, img TEXT, nomeDoador TEXT, cidadeDoador TEXT, telefoneDoador INTEGER, emailDoador TEXT, id_adotante TEXT, FOREIGN KEY (id_adotante) REFERENCES adotante (cpf))'],          
           ['CREATE TABLE IF NOT EXISTS adotante (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome TEXT, endereco TEXT, bairro TEXT, cidade TEXT, telefone INTEGER, email TEXT, rg TEXT, cpf TEXT, Q1 TEXT, Q2 TEXT, Q3 TEXT, Q4 TEXT, Q5 TEXT, Q6 TEXT, Q7 TEXT, Q8 TEXT)']
         ])
@@ -34,11 +35,11 @@ export class DatabaseProvider {
       }
 
       private insertDefaultItems(db: SQLiteObject){
-        db.executeSql('select COUNT(id) as qtd from animal', {})
+        db.executeSql('select COUNT(id) as qtd from usuario', {})
         .then((data: any) =>{
           if (data.rows.item(0).qtd == 0){
             db.sqlBatch([
-              ['INSERT INTO animal (status, tipo, nome, sexo, anos, meses, porte, temperamento, raca, vacinado, castrado, info, img) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', ['Pending', 'cão', 'pan1', 'F', 7, 5, 'pequeno', 'dócil', 'SRD', 'sim', 'sim', 'nenhuma info', 'img']]
+              ['INSERT INTO animal (login, senha) VALUES (?, ?)', ['admin', 'admin']]
             ])
             .then(() => console.log('Dados padrões incluídos'))
             .catch(e => console.error('Erro ao incluir dados padrões', e));
